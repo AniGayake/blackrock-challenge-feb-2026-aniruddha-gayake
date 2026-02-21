3d1ddd0da56ee9dbc35e6632e8331b2dae3a6251
Aniruddha Gayake <anigayake101@gmail.com>
2026-02-21 16:32:45 +0530

    formatted

--- src/main/java/com/blackrock/challenge/service/TransactionCoreService.java
+++ src/main/java/com/blackrock/challenge/service/TransactionCoreService.java
@@ -17,16 +17,16 @@ import java.util.stream.Collectors;
 public class TransactionCoreService {
 
     public List<TransactionResponse> enrichTransactions(@Valid final List<ExpensesRequest> expensesList) {
-       return expensesList.stream()
+        return expensesList.stream()
                 .map(
-                        expense->{
+                        expense -> {
                             TransactionResponse enrichedTransactionResponse = new TransactionResponse();
                             Double amount = expense.getAmount();
-                            Double ceiling = Math.ceil((amount/100))*100;
+                            Double ceiling = Math.ceil((amount / 100)) * 100;
                             enrichedTransactionResponse.setAmount(expense.getAmount());
                             enrichedTransactionResponse.setDate(expense.getDate());
                             enrichedTransactionResponse.setCeiling(ceiling);
-                            enrichedTransactionResponse.setRemanent(ceiling-amount);
+                            enrichedTransactionResponse.setRemanent(ceiling - amount);
                             return enrichedTransactionResponse;
                         }
                 ).collect(Collectors.toList());
@@ -35,7 +35,7 @@ public class TransactionCoreService {
     public TransactionValidationResponse validateTransactions(final TransactionValidationRequest transactionValidationRequest) {
         TransactionValidationResponse transactionValidationResponse = new TransactionValidationResponse();
         Set<Transaction> seen = new HashSet<>();
-        Set<Transaction> invalidSeen= new HashSet<>();
+        Set<Transaction> invalidSeen = new HashSet<>();
 
         List<Transaction> transactionsList = transactionValidationRequest.getTransactions();
 
@@ -54,7 +54,7 @@ public class TransactionCoreService {
     public TransactionFilterResponse filterTransactions(@Valid TransactionFilterRequest transactionFilterRequest) {
         List<Transaction> transactionsList = transactionFilterRequest.getTransactions();
         Set<Transaction> seen = new HashSet<>();
-        Set<Transaction> invalidSeen= new HashSet<>();
+        Set<Transaction> invalidSeen = new HashSet<>();
         List<KPeriod> kPeriod = transactionFilterRequest.getK();
 
         List<InvalidTransaction> invalidTransactions = getInvalidTransactions(transactionsList, invalidSeen);
@@ -63,11 +63,11 @@ public class TransactionCoreService {
 
 
         List<Transaction> valid = seen.stream()
-                .filter(t->isInAnyKPeriod(t.getDate(),kPeriod))
+                .filter(t -> isInAnyKPeriod(t.getDate(), kPeriod))
                 .toList();
         List<ValidTransactionFilterResponse> validTransactionFilterResponses = new ArrayList<>();
 
-        for (Transaction t:valid){
+        for (Transaction t : valid) {
             ValidTransactionFilterResponse response = new ValidTransactionFilterResponse();
             response.setInKPeriod(true);
             response.setAmount(t.getAmount());
@@ -82,30 +82,33 @@ public class TransactionCoreService {
         response.setInvalid(invalidTransactions);
         return response;
     }
+
     private boolean isInAnyKPeriod(LocalDateTime txDate, List<KPeriod> kPeriods) {
         if (kPeriods == null || kPeriods.isEmpty()) return false;
         return kPeriods.stream()
                 .anyMatch(k -> isInRange(txDate, k.getStart(), k.getEnd()));
     }
+
     private boolean isInRange(LocalDateTime date, LocalDateTime start, LocalDateTime end) {
         return !date.isBefore(start) && !date.isAfter(end);
     }
-    private  List<Transaction> getDuplicateTransactions(List<Transaction> transactionsList, Set<Transaction> invalidSeen, Set<Transaction> seen) {
-        List<Transaction> duplicateTransactions=
+
+    private List<Transaction> getDuplicateTransactions(List<Transaction> transactionsList, Set<Transaction> invalidSeen, Set<Transaction> seen) {
+        List<Transaction> duplicateTransactions =
                 transactionsList
                         .stream()
-                        .filter(t->!invalidSeen.contains(t))
-                        .filter(t->!seen.add(t))
+                        .filter(t -> !invalidSeen.contains(t))
+                        .filter(t -> !seen.add(t))
                         .toList();
         return duplicateTransactions;
     }
 
-    private  List<InvalidTransaction> getInvalidTransactions(List<Transaction> transactionsList, Set<Transaction> invalidSeen) {
+    private List<InvalidTransaction> getInvalidTransactions(List<Transaction> transactionsList, Set<Transaction> invalidSeen) {
         List<InvalidTransaction> invalidTransactions =
                 transactionsList.
                         stream()
-                        .filter(t->t.getAmount()<0||t.getRemanent()>t.getCeiling())
-                        .map(t->{
+                        .filter(t -> t.getAmount() < 0 || t.getRemanent() > t.getCeiling())
+                        .map(t -> {
                             InvalidTransaction invalidTransaction = new InvalidTransaction();
                             invalidTransaction.setAmount(t.getAmount());
                             invalidTransaction.setCeiling(t.getCeiling());
